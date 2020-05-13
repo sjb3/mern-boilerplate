04eac87a6023bd851b8e7085948e664d4a4e332c
Justin Byun <klofseattle@hotmail.com>
2020-05-12 20:15:38 -0700

    Fix routes and corresponding methods

--- models/User.js
+++ models/User.js
@@ -1,6 +1,11 @@
 "use strict";
 
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
+const moment = require("moment");
+
 const userSchema = mongoose.Schema({
   firstname: {
     type: String,
@@ -13,7 +18,7 @@ const userSchema = mongoose.Schema({
   },
   password: {
     type: String,
-    minlength: 5,
+    minglength: 5,
   },
   lastname: {
     type: String,
@@ -23,6 +28,7 @@ const userSchema = mongoose.Schema({
     type: Number,
     default: 0,
   },
+  image: String,
   token: {
     type: String,
   },
@@ -30,6 +36,56 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+userSchema.pre("save", function (next) {
+  var user = this;
+
+  if (user.isModified("password")) {
+    // console.log('password changed')
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (cb) {
+  var user = this;
+  var token = jwt.sign(user._id.toHexString(), "secret");
+  var oneHour = moment().add(1, "hour").valueOf();
+
+  user.tokenExp = oneHour;
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
+
+userSchema.statics.findByToken = function (token, cb) {
+  var user = this;
+
+  jwt.verify(token, "secret", function (err, decode) {
+    user.findOne({ _id: decode, token: token }, function (err, user) {
+      if (err) return cb(err);
+      cb(null, user);
+    });
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 
