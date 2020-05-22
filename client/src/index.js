794ea5113cbc4f3b6ff3c5ab719979e936364916
Justin Byun <klofseattle@hotmail.com>
2020-05-22 02:24:50 -0700

    Refactor logIn page with class component

--- client/src/index.js
+++ client/src/index.js
@@ -3,31 +3,33 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import "materialize-css/dist/css/materialize.min.css";
 
-import Reducer from "./_reducers";
+// import Reducer from "./_reducers";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import promiseMiddleware from "redux-promise";
 import ReduxThunk from "redux-thunk";
 
-const createStoreWithMiddleware = applyMiddleware(
-  promiseMiddleware,
-  ReduxThunk,
-)(createStore);
+// const createStoreWithMiddleware = applyMiddleware(
+//   promiseMiddleware,
+//   ReduxThunk,
+// )(createStore);
 
-ReactDOM.render(
-  <Provider
+{
+  /* <Provider
     store={createStoreWithMiddleware(
       Reducer,
       window.__REDUX_DEVTOOLS_EXTENSION__ &&
         window.__REDUX_DEVTOOLS_EXTENSION__(),
     )}
-  >
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById("root"),
+  > */
+}
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
